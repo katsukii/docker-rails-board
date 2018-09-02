cbd9813a8aac3d271c4bba5776763cd9635a5653
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-09-02 13:35:33 +0900

    kaminariでページネーション

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,7 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
   def index
-    @boards = Board.all
+    @boards = Board.page(params[:page])
 	end
 
   def new
@@ -8,28 +9,36 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
-  end
-
-  def show
-    @board = Board.find(params[:id])
-  end
-
-  def edit
-    @board = Board.find(params[:id])
-  end
-
-  def update
-    board = Board.find(params[:id])
-    board.update(board_params)
-
+    board = Board.create(board_params)
     redirect_to board
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @board.update(board_params)
+
+    redirect_to @board
+  end
+
+  def destroy
+    @board.delete
+
+    redirect_to boards_path
+  end
 
   private
 
   def board_params
     params.require(:board).permit(:name, :title, :body)
   end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+
 end
