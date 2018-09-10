0573d317a40fe41fbf53a5450ebfa2f8b2b0c7b7
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-09-10 23:27:46 +0900

    コメントの投稿処理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,21 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_to comment.board
+    else
+      redirect_to :back, flash: {
+        comment: comment,
+        error_messages: comment.errors.full_mesages
+      }
+    end
   end
 
   def destroy
+  end
+
+  def comment_params
+    params.require(:comment).permit(:board_id, :name, :comment)
   end
 end
