32bf74fefaa9007bf01e09128b04b3b08a3232ab
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-09-18 08:43:01 +0900

    タグ検索

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+ config.log_level = :debug
+ config.log_formatter = Logger::Formatter.new
+
+
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
